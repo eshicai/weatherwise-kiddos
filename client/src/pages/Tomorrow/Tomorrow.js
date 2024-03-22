e60cfe7279a4c3a3967ef8b20ac8d092c10303ac
eshicai <eshicai@gmail.com>
2024-03-22 16:37:37 -0700

    style link today to arrow back in tomorrow page

--- client/src/pages/Tomorrow/Tomorrow.js
+++ client/src/pages/Tomorrow/Tomorrow.js
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 import { Header } from '../../components/Header/Header';
 import { TomorrowWeather } from '../../components/TomorrowWeather/TomorrowWeather';
 import { Slogan } from '../../components/Slogan/Slogan';
+import { HeadingWithBackArrow } from '../../components/HeadingWithBackArrow/HeadingWithBackArrow';
 
 export const Tomorrow = () => {
   return (
     <div className="tomorrow-page">
       <Header />
       <main className='main'>
-        <Link to='/'>Today</Link>
+        {/* <Link to='/'>Today</Link> */}
+        <div className="tomorrow-page__heading-container">
+          <HeadingWithBackArrow
+            link="/"
+            heading="Today"
+          />
+        </div>
         <TomorrowWeather />
         <Slogan />
       </main>
