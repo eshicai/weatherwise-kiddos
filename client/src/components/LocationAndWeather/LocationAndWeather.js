eeb50787e4989badae3c6a858f92a0e5bb30cb84
eshicai <eshicai@gmail.com>
2024-03-26 09:40:26 -0700

    display essentials images

--- client/src/components/LocationAndWeather/LocationAndWeather.js
+++ client/src/components/LocationAndWeather/LocationAndWeather.js
@@ -8,17 +8,14 @@ export const LocationAndWeather = () => {
   // default location: Toronto
   const defaultLatitude = 43.64780785016635;
   const defaultLongitude = -79.39656626973078;
-  const defaultTimezoneOffset = 240;
 
   const storedLatitude = sessionStorage.getItem('latitude');
   const storedLongitude = sessionStorage.getItem('longitude');
-  const storedTimezoneOffset = sessionStorage.getItem('timezoneOffset');
   const buttonClicked = sessionStorage.getItem('locationButtonClicked');
 
   const [location, setLocation] = useState(null);
   const [latitude, setLatitude] = useState(storedLatitude || defaultLatitude);
   const [longitude, setLongitude] = useState(storedLongitude || defaultLongitude);
-  const [timezoneOffset, setTimezoneOffset] = useState(storedTimezoneOffset || defaultTimezoneOffset);
   const [showButton, setShowButton] = useState(buttonClicked !== 'true');
   const [buttonConfirm, setButtonConfirm] = useState(false);
   const [getLocation, setGetLocation] = useState(false);
@@ -51,7 +48,6 @@ export const LocationAndWeather = () => {
       }
 
       const timezoneOffset = (new Date()).getTimezoneOffset();
-      setTimezoneOffset(timezoneOffset);
       sessionStorage.setItem('timezoneOffset', timezoneOffset);
     }
 
@@ -73,11 +69,11 @@ export const LocationAndWeather = () => {
       ) : null}
       {buttonConfirm && <Location setButtonConfirm={setButtonConfirm} setGetLocation={setGetLocation} />}
 
-      {/* {currentHour < 17 ?
+      {currentHour < 17 ?
         <ForecastWeather dateOffset={dateOffset} />
-        : */}
+        :
         <TodayWeather className='location__weather' latitude={latitude} longitude={longitude} />
-      {/* } */}
+      }
     </div>
   );
 }
